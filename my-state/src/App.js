695078165d7a466f7d4ac512322567ce3e7af318
Code Shivam <pandeeyy294@gmail.com>
2021-12-22 10:33:39 +0530

    react state basics

---
+++ my-state/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, {Component} from 'react';
+
+export class App extends Component{
+  state ={
+    name :"Shivam",
+    roll : "102",
+    batch : this.props.Batch
+  }
+
+  render(){
+    return(
+      <div className='App'>
+        <h1>Hi {this.state.name}, your roll no is 
+        {this.state.roll}
+        <h2>Your batch is {this.state.batch}</h2>
+        </h1>
+      </div>
+    );
+  }
+}
